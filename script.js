c65c79f8e096d3ad03188ac1a58b0c3dcc5df56d
muhammedmiracozkan06-pixel <muhammedmiracozkan06@gmail.com>
2026-03-17 20:23:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const modelSelect = document.getElementById('model-select');
 let isSending = false;
 
 // API CONFIGURATION
-const GROQ_API_KEY = "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";
+const GROQ_API_KEY = "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";
 
 async function getAIResponse(prompt) {
     try {
